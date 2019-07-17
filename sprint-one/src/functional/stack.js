5ab0e5fe8d93b845abd854aea0aa59660fdfd080
ez-li <eileenli.sf@gmail.com>
2019-07-17 12:41:39 -0700

    finished functional, stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,30 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  // Define global variable to hold count of keuys
+  var counter = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[counter] = value;
+    counter ++;
   };
 
   someInstance.pop = function() {
+    var popped = storage[counter-1];
+    delete storage[counter-1];
+    counter --;
+    return popped;
   };
 
   someInstance.size = function() {
+    var length = 0;
+    for (var key in storage) {
+      length++;
+    }
+    return length;
   };
 
   return someInstance;
 };
+
+
